b1b9e9826fe2f18be6d479cc16f5e1ebc9970bd3
Dexter Rivas <drivas@cs.utsa.edu>
2011-11-15 10:35:16 -0600

    spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -22,10 +22,10 @@ require 'spec_helper'
     response.should have_selector("title", :content => "Help")
   end
 
-  #it "should have a sign-up page at '/signup'" do
-  #  get '/signup'
-  #  response.should have_selector("title", :content => "Sign up")
-  #end
+  it "should have a sign-up page at '/signup'" do
+    get '/signup'
+    response.should have_selector("title", :content => "Sign up")
+  end
 
   it "should have the right links on the layout" do
     visit root_path
@@ -36,8 +36,8 @@ require 'spec_helper'
     response.should have_selector('title', :content => "Contact")
     click_link "Home"
     response.should have_selector('title', :content => "Home")
-    #click_link "Sign up now"
-    #response.should have_selector('title', :content => "Sign up")
+    click_link "Sign up now"
+    response.should have_selector('title', :content => "Sign up")
     response.should have_selector('a[href="/"]>img')
   end
 end
