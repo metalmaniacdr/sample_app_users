9ef24dff58cff60a267c0096dfad1f33eefb7b85
Dexter Rivas <drivas@cs.utsa.edu>
2011-11-15 10:37:49 -0600

    put render_views in integrations test

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,8 @@
 require 'spec_helper'
 
- describe "LayoutLinks" do
-
+describe "LayoutLinks" do
+  render_views
+  
   it "should have a home page at '/'" do
     get '/'
     response.should have_selector("title", :content => "Home")
